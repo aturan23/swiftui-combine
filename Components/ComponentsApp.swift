60c15716e886a897a6e60850a490be3e24e05d74
Turan Assylkhan <turan@strongte.am>
2020-11-16 16:49:59 +0600

    feat: init

---
+++ Components/ComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComponentsApp.swift
+//  Components
+//
+//  Created by Turan Assylkhan on 11/16/20.
+//
+
+import SwiftUI
+
+@main
+struct ComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
